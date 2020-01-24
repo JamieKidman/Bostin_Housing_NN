0683e8b64d1a7e6e116c1e2c368f62d3a27ca45a
Jamie Kidman <44501486+JamieKidman@users.noreply.github.com>
2020-01-25 01:18:40 +0800

    Update Bostin_Housing_NN.py
    Altered spacing from jupyter notebook to normal python

--- Bostin_Housing_NN.py
+++ Bostin_Housing_NN.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import numpy as np
 import tensorflow as tf
 from keras.datasets import boston_housing
@@ -33,11 +27,10 @@ http://lib.stat.cmu.edu/datasets/boston
  """
 
 # The dataset is complete and doesnt have missing values.
-# There are moral and potentially legal issues using B so the data will be removed
 
+# There are moral and potentially legal issues using B so the data will be removed
 x_train = np.delete(x_train, 11, axis=1)
 x_test = np.delete(x_test, 11, axis=1)
-
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Dense(12, input_dim=12, activation=tf.nn.elu))
@@ -45,39 +38,21 @@ model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(12, activation=tf.nn.relu))
 
-model.add(tf.keras.layers.Dense(6, activation=tf.nn.relu))
-
 model.add(tf.keras.layers.Dense(1, kernel_initializer='normal'))
+
 
 model.compile(loss="mean_squared_error", optimizer="RMSprop", metrics=["mean_absolute_error"])
 
 model.fit(x_train, y_train, epochs=5000, use_multiprocessing=True)
 
 
-# In[38]:
-
-
 model.evaluate(x_test, y_test)
-
-
-# In[39]:
-
 
 model.test_on_batch(x_test, y_test)
 var = model.predict(x_test)
-
-
-# In[40]:
 
 
 for i, val in enumerate(y_test):
     diff = abs(val - var[i])
     
     print("index: {}\t {:5}  :  {}\t\t difference: {}".format(i, val, var[i], diff))
-
-
-# In[ ]:
-
-
-
-
